f94f533d098c5828866f3e5bbef194c423dde766
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-13 15:05:31 +0900

    ✨ :: PasswordEncoder 추가

---
+++ src/main/kotlin/com/example/blendings_backend/global/security/config/PasswordEncoderConfig.kt
@@ -0,0 +1,12 @@
+package com.example.blendings_backend.global.security.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+@Configuration
+class PasswordEncoderConfig {
+
+    @Bean
+    protected fun passwordEncoder(): BCryptPasswordEncoder = BCryptPasswordEncoder()
+}
