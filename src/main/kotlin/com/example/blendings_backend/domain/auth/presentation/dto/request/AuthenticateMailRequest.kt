c8994a7b78a320774c2606c6edc8fbe2de960720
Daybreak312 <ty82afg12@dsm.hs.kr>
2023-12-15 19:26:19 +0900

    ♻ :: Validation 관련 어노테이션

--- src/main/kotlin/com/example/blendings_backend/domain/auth/presentation/dto/request/AuthenticateMailRequest.kt
+++ src/main/kotlin/com/example/blendings_backend/domain/auth/presentation/dto/request/AuthenticateMailRequest.kt
@@ -1,6 +1,16 @@
 package com.example.blendings_backend.domain.auth.presentation.dto.request
 
+import com.example.blendings_backend.domain.auth.presentation.AuthValidationValue
+import javax.validation.constraints.Pattern
+
 data class AuthenticateMailRequest(
+
+    @Pattern(regexp = AuthValidationValue.MAIL_REGEXP, message = AuthValidationValue.MAIL_MESSAGE)
     val mail: String,
+
+    @Pattern(
+        regexp = AuthValidationValue.AUTHENTICATION_CODE_REGEXP,
+        message = AuthValidationValue.AUTHENTICATION_CODE_MESSAGE
+    )
     val authenticationCode: String
 )
