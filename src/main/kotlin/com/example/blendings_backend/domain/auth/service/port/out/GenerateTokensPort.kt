0fbc20f4ce6ec6916dc279bcd2ab4511b755c935
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-14 19:43:37 +0900

    🔥 :: 임시 파일 삭제

--- src/main/kotlin/com/example/blendings_backend/domain/auth/service/port/out/GenerateTokensPort.kt
+++
@@ -1,8 +0,0 @@
-package com.example.blendings_backend.domain.auth.service.port.out
-
-import com.example.blendings_backend.domain.auth.service.dto.TokenResponse
-
-interface GenerateTokensPort {
-
-    fun generateTokens(subject: Any): TokenResponse
-}
--- src/main/kotlin/com/example/blendings_backend/global/security/jwt/adapter/JwtProvideAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.blendings_backend.global.security.jwt.adapter
-
-import com.example.blendings_backend.global.annotation.Adapter
-
-@Adapter
-class JwtProvideAdapter {
-}
