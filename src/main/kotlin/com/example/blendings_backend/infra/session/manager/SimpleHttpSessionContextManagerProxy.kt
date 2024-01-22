3a07ae57c05d9d18a0c2b01d5583a88f742693e2
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-23 03:20:36 +0900

    ✨ :: Session Manager 추가

---
+++ src/main/kotlin/com/example/blendings_backend/infra/session/manager/SimpleHttpSessionContextManagerProxy.kt
@@ -0,0 +1,52 @@
+package com.example.blendings_backend.infra.session.manager
+
+import org.springframework.context.annotation.Primary
+import org.springframework.stereotype.Component
+import javax.servlet.http.HttpSession
+
+@Primary
+@Component
+class SimpleHttpSessionContextManagerProxy : SimpleHttpSessionContextManager() {
+
+    private companion object {
+        lateinit var sessionContextSave: MutableMap<String, HttpSession>
+    }
+
+    override fun addSession(httpSession: HttpSession) {
+        try {
+            super.addSession(httpSession)
+            commit()
+        } catch (e: Exception) {
+            rollback()
+            throw e
+        }
+    }
+
+    override fun removeSession(httpSession: HttpSession) {
+        try {
+            super.removeSession(httpSession)
+            commit()
+        } catch (e: Exception) {
+            rollback()
+            throw e
+        }
+    }
+
+    override fun removeSessionByUserMailAddress(mailAddress: String) {
+        try {
+            super.removeSessionByUserMailAddress(mailAddress)
+            commit()
+        } catch (e: Exception) {
+            rollback()
+            throw e
+        }
+    }
+
+    private fun commit() {
+        sessionContextSave = sessionContext
+    }
+
+    private fun rollback() {
+        sessionContext = sessionContextSave
+    }
+}
