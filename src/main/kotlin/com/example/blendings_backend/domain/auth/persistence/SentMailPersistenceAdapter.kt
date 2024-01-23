cfe29fb463b386c059cc0e3475a58609d3c4212b
Daybreak312 <ty82afg12@dsm.hs.kr>
2024-01-23 14:45:52 +0900

    ♻

--- src/main/kotlin/com/example/blendings_backend/domain/auth/persistence/SentMailPersistenceAdapter.kt
+++ src/main/kotlin/com/example/blendings_backend/domain/auth/persistence/SentMailPersistenceAdapter.kt
@@ -2,11 +2,10 @@ package com.example.blendings_backend.domain.auth.persistence
 
 import com.example.blendings_backend.domain.auth.persistence.mapper.SentMailMapper
 import com.example.blendings_backend.domain.auth.persistence.repository.SentMailRepository
-import com.example.blendings_backend.domain.auth.service.vo.SentMailModel
 import com.example.blendings_backend.domain.auth.service.port.out.persistence.DeleteSentMailByMailAddressPort
-import com.example.blendings_backend.domain.auth.service.port.out.persistence.FindSentMailByAuthenticationCodePort
 import com.example.blendings_backend.domain.auth.service.port.out.persistence.FindSentMailByMailAddressPort
 import com.example.blendings_backend.domain.auth.service.port.out.persistence.SaveSentMailPort
+import com.example.blendings_backend.domain.auth.service.vo.SentMailModel
 import com.example.blendings_backend.global.annotation.PersistenceAdapter
 
 @PersistenceAdapter
